b9bf42d8bebcaa4843c087b603bc2d8261160a25
Alessandro Isaacs <dr.ahfz@gmail.com>
2015-07-13 08:59:32 -0700

    Self-managing queue and test

--- test/spec/ol/tilequeue.test.js
+++ test/spec/ol/tilequeue.test.js
@@ -1,6 +1,8 @@
 goog.provide('ol.test.TileQueue');
 
-describe('ol.TileQueue', function() {
+describe
+
+('ol.TileQueue', function() {
 
   function addRandomPriorityTiles(tq, num) {
     var i, tile, priority;
@@ -12,6 +14,88 @@ describe('ol.TileQueue', function() {
       tq.queuedElements_[tile.getKey()] = true;
     }
   }
+
+  var tileId = 0;
+  function createImageTile() {
+    ++tileId;
+    var tileCoord = [tileId, tileId, tileId];
+    var state = ol.TileState.IDLE;
+    var src = 'data:image/gif;base64,R0lGODlhAQABAPAAAP8AAP///' +
+        'yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==#' + tileId;
+
+    return new ol.ImageTile(tileCoord, state, src, null,
+        ol.source.Image.defaultImageLoadFunction);
+  }
+
+  describe('enqueue()', function() {
+
+    var noop = function() {};
+
+    it('Enqueues the tiles and loads them', function(done) {
+      var q = new ol.TileQueue(noop, noop);
+
+      var numTiles = 20;
+      var maxLoading = 2;
+      var maxNewLoads = 2;
+
+      q.loadMoreTiles(maxLoading, maxNewLoads);
+
+      for (var i = 0; i < numTiles; ++i) {
+        var tile = createImageTile();
+        q.enqueue([tile]);
+      }
+
+      expect(q.getCount()).to.equal(numTiles - Math.min(maxLoading, maxNewLoads));
+      expect(q.getTilesLoading()).to.equal(Math.min(maxLoading, maxNewLoads));
+
+      setTimeout(function() {
+
+        expect(q.getCount()).to.equal(0);
+        expect(q.getTilesLoading()).to.equal(0);
+
+        done();
+      }, 20);
+    });
+  });
+
+  describe('#loadMoreTiles()', function() {
+    var noop = function() {};
+
+    it('works when tile queues share tiles', function(done) {
+      var q1 = new ol.TileQueue(noop, noop);
+      var q2 = new ol.TileQueue(noop, noop);
+
+      var maxLoading = 2;
+      var maxNewLoads = 2;
+
+      q1.loadMoreTiles(maxLoading, maxNewLoads);
+      q2.loadMoreTiles(maxLoading, maxNewLoads);
+
+      var numTiles = 20;
+      for (var i = 0; i < numTiles; ++i) {
+        var tile = createImageTile();
+        q1.enqueue([tile]);
+        q2.enqueue([tile]);
+      }
+
+      // Since loading starts immediately, some tiles will be loading
+      expect(q1.getCount()).to.equal(numTiles - Math.min(maxLoading, maxNewLoads));
+      expect(q2.getCount()).to.equal(numTiles - (Math.min(maxLoading, maxNewLoads) * 2));
+
+      // let all tiles load
+      setTimeout(function() {
+        expect(q1.getTilesLoading()).to.equal(0);
+        expect(q2.getTilesLoading()).to.equal(0);
+
+        expect(q1.getCount()).to.equal(0);
+        expect(q2.getCount()).to.equal(0);
+
+        done();
+      }, 20);
+
+    });
+
+  });
 
   describe('heapify', function() {
     it('does convert an arbitrary array into a heap', function() {
